ffa1d6e8b0ee178137be89a5756e0aaea05b18b0
Bas Nguyen <basdnguyen@gmail.com>
2024-06-28 17:14:49 +0700

    say hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import Image from "next/image";
+'use client';
+
+import { Box, Container, Typography, useTheme } from "@mui/material";
 
 export default function Home() {
+  const theme = useTheme();
   return (
-    <div>Hello</div>
+    <Container maxWidth={false} sx={{ minHeight: '100vh', backgroundColor: theme.palette.background.default }} disableGutters>
+      <Box sx={{
+        height: 300,
+        backgroundColor: theme.palette.primary.dark,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <Typography variant="h1" color={theme.palette.getContrastText(theme.palette.primary.dark)}>Hello! I'm Bang Nguyen,</Typography>
+        <Typography variant="h2" color={theme.palette.getContrastText(theme.palette.primary.dark)}>Web Developer</Typography>
+      </Box>
+    </Container>
   );
 }

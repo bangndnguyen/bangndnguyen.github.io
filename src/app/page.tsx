62a4669224448f0963f9b2a02f5c8342492f0c27
Bas Nguyen <basdnguyen@gmail.com>
2024-06-28 17:47:18 +0700

    better title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,24 @@ import { Box, Container, Typography, useTheme } from "@mui/material";
 export default function Home() {
   const theme = useTheme();
   return (
-    <Container maxWidth={false} sx={{ minHeight: '100vh', backgroundColor: theme.palette.background.default }} disableGutters>
-      <Box sx={{
-        height: 300,
-        backgroundColor: theme.palette.primary.dark,
+    <Container maxWidth={false} sx={{
+      minHeight: '100vh', backgroundColor: theme.palette.background.paper,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center'
+    }} disableGutters>
+      <Container maxWidth='sm' sx={{
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
         justifyContent: 'center'
       }}>
-        <Typography variant="h1" color={theme.palette.getContrastText(theme.palette.primary.dark)}>Hello! I&apos;m Bang Nguyen,</Typography>
-        <Typography variant="h2" color={theme.palette.getContrastText(theme.palette.primary.dark)}>Web Developer</Typography>
-      </Box>
+        <Typography variant="h3" color={theme.palette.getContrastText(theme.palette.background.paper)}>
+          Bang Nguyen
+        </Typography>
+        <Typography variant="subtitle1" color={theme.palette.getContrastText(theme.palette.background.paper)} marginTop={3}>
+          Hello! I&apos;m a Senior Web Developer based in Ho Chi Minh City, Vietnam. I&apos;m looking for an opportunity to work in Germany. Here is a showcase of what I can do. Hit me up if you&apos;re interested.
+        </Typography>
+      </Container>
     </Container>
   );
 }

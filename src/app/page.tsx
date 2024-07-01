dba28e07eaccb63ee4d216dbaced0a64448d9d2a
Bas Nguyen <basdnguyen@gmail.com>
2024-07-01 10:33:49 +0700

    add avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Chip, Container, Typography, useTheme } from "@mui/material";
+import { Avatar, Box, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Chip, Container, Typography, useTheme } from "@mui/material";
 import DescriptionIcon from '@mui/icons-material/Description';
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -12,11 +12,14 @@ export default function Home() {
   return (
     <Container maxWidth='md'>
       <Grid container minHeight={'100vh'} alignItems={'center'}
-        columnSpacing={{ sm: 6 }} rowSpacing={{ xs: 6, sm: 0 }} paddingY={6}>
-        <Grid sm={6} justifyContent={{ xs: 'start', sm: 'end' }} alignContent={'center'} container>
-          <Typography variant="h3" color={theme.palette.getContrastText(theme.palette.background.paper)}>
-            BANG NGUYEN
-          </Typography>
+        spacing={{ sm: 8 }} rowSpacing={{ xs: 6, sm: 0 }} paddingY={6}>
+        <Grid sm={6} flexDirection={'column'} alignItems={{ xs: 'start', sm: 'end' }} alignContent={'center'} container>
+          <Grid container flexDirection={'column'} alignItems={'center'} gap={4}>
+            <Avatar src='/avatar.jpg' sx={{ width: 256, height: 256 }} />
+            <Typography variant="h3" color={theme.palette.getContrastText(theme.palette.background.paper)}>
+              BANG NGUYEN
+            </Typography>
+          </Grid>
           <Typography
             variant="subtitle1"
             color={theme.palette.getContrastText(theme.palette.background.paper)}
@@ -43,38 +46,45 @@ export default function Home() {
             </Button>
           </Box>
         </Grid>
-        <Grid sm={6} alignContent={'center'} paddingTop={{ xs: 6, sm: 0 }}>
-          <Card sx={{ maxWidth: 400 }}>
-            <CardHeader
-              title="Petbook"
-            />
-            <CardMedia
-              component="img"
-              height="194"
-              image="/petbook-demo.png"
-              alt="Paella dish"
-            />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                A social network where pets are superstars.
-              </Typography>
-              <Box sx={{ display: 'flex', gap: 0.5, paddingTop: 1, flexWrap: 'wrap' }}>
-                <Chip variant="outlined" label='React' />
-                <Chip variant="outlined" label='NextJS' />
-                <Chip variant="outlined" label='MaterialUI' />
-                <Chip variant="outlined" label='Postgres' />
-                <Chip variant="outlined" label='REST APIs' />
-              </Box>
-            </CardContent>
-            <CardActions disableSpacing>
-              <Button startIcon={<GitHubIcon />} href='https://github.com/basdnguyen/petbook' target='_blank'>
-                Github
-              </Button>
-              <Button startIcon={<LaunchIcon />} href='https://petbook.bangnguyen.me/' target='_blank'>
-                Demo
-              </Button>
-            </CardActions>
-          </Card>
+        <Grid sm={6} alignContent={'center'} paddingTop={{ xs: 6, sm: 0 }} container>
+          <Grid>
+            <Typography variant="h5">
+              Skill Demos
+            </Typography>
+          </Grid>
+          <Grid paddingTop={3}>
+            <Card sx={{ maxWidth: 512 }}>
+              <CardHeader
+                title="Petbook"
+              />
+              <CardMedia
+                component="img"
+                height="194"
+                image="/petbook-demo.png"
+                alt="Paella dish"
+              />
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  A social network where pets are superstars.
+                </Typography>
+                <Box sx={{ display: 'flex', gap: 0.5, paddingTop: 1, flexWrap: 'wrap' }}>
+                  <Chip variant="outlined" label='React' />
+                  <Chip variant="outlined" label='NextJS' />
+                  <Chip variant="outlined" label='MaterialUI' />
+                  <Chip variant="outlined" label='Postgres' />
+                  <Chip variant="outlined" label='REST APIs' />
+                </Box>
+              </CardContent>
+              <CardActions disableSpacing>
+                <Button startIcon={<GitHubIcon />} href='https://github.com/basdnguyen/petbook' target='_blank'>
+                  Github
+                </Button>
+                <Button startIcon={<LaunchIcon />} href='https://petbook.bangnguyen.me/' target='_blank'>
+                  Demo
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
       </Grid>
     </Container>
